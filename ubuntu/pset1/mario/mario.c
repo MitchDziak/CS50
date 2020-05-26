d05b4bd4aceef4897fc8f9ec798e3bc398371325
Mitchell Dziak <42303636+MitchDziak@users.noreply.github.com>
2020-05-26 00:06:03 -0500

    Add files via upload

--- ubuntu/pset1/mario/mario.c
+++ ubuntu/pset1/mario/mario.c
@@ -5,13 +5,13 @@ int main(void)
 {
     //Getting the height and width of the first row and column
     int h = get_int("What height would you like? ");
-    
+
     //Error case, if user inputs an invalid number, it will reprompt
     while (h < 1 || h > 8)
     {
         h = get_int("What height would you like? (Please use a positive integer!) ");
     }
-    
+
     //This loop will run as long as the input is more than 0
     while (h > 0)
     {
@@ -19,12 +19,27 @@ int main(void)
         //while i is greater than 0, it will print an # for each h and decrement for each loop
         for (int i = h; i > 0; i--)
         {
-            printf("#");
+            printf(".");
         }
+
+        int hash = 1;
+        if (h > 0)
+        {
+            for (hash = 0; hash > -1; hash++)
+            {
+                printf("#");
+                if (hash > h)
+                {
+                    break;
+                }
+        }
+
         //While h is greater than 0, each time i decrements, a new line is added and h is decremented as well
         //When the loop restarts, as long as h > 0, the new h will be used in the for loop
         //This creates a pyramid shape, but it isn't correctly done as the problem set asks
         printf("\n");
+
         h--;
+        }
     }
 }
